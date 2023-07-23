addc41621b10bda80f724e90c6d233e749a38df6
2dogsandaguy <Neverclear@ymail.com>
2023-07-23 17:19:07 -0400

    trying to link button with javascript

--- assets/style.js
+++ assets/style.js
@@ -1,28 +1,33 @@
 const start = document.querySelector(".start")
-var questionsElement = document.getElementById("questions-show")
-const selectChoiceElement = document.getElementById("select-choice")
-var choiceButtons = document.getElementById(".btn")
-let questions =[
-    {
-        question: 'waht is 2 + 2',
-        answers: [
-            {Text: '4', correct: true},
-            {Text: '22', correct: false},
-            {Text: '222', correct: false},
-            {Text: '900', correct: false},
-        ]
-    },
-    {
-        question: 'waht is 8 + 2',
-        answers: [
-            {Text: '10', correct: true},
-            {Text: '82', correct: false},
-            {Text: '282', correct: false},
-            {Text: '8+2', correct: false},
-        ]
-    }
 
+var questionsElement = document.getElementById("questions-show")
+
+var selectChoiceElement = document.getElementById("select-choice")
+
+var choiceButtons = document.getElementById(".btn")
+
+let questions = [
+    {
+        title: 'where was the first ever comic con held on record ? ',
+        answers: [
+             'New York city',
+            'seattle',
+            'sandigio',
+            '900'] ,
+            correctAnnswer: 0
+        
+    },   {
+        title: 'where was the first ever comic con held on record ? ',
+        answers: [
+             'New York city',
+            'seattle',
+            'sandigio',
+            '900'] ,
+            correctAnnswer: 0
+        
+    }
 ]
+
 
 
 function startQuiz() { 
@@ -30,7 +35,7 @@ function startQuiz() {
     element.classList.add("secshow");
     start.style.display = "none";
     showQuestions()
-    showChoices()
+    
     console.log(startQuiz)
 }
 function getQuestion() {
@@ -38,13 +43,21 @@ function getQuestion() {
 
 }
 function showQuestions(){
-    questionsElement.textContent = questions[0].question;
-console.log(showQuestions);
+    questionsElement.textContent = questions[0].title;
+     
+     let choice = document.querySelectorAll('.btn');
+     console.log(choice);
+    choice.forEach(function(element, index){
+     element.textContent = "huge cock"
+     });
+     console.log(showQuestions);
 }
 function showChoices(){
-selectChoiceElement.textContent = questions[0].answers;
-     for(var i = 0; i < answers.lenght; i++) {
-        selectChoiceElement.textContent = answers[i]
-     }
+//selectChoiceElement.textContent = questions[0].answers;
+    //answers = questions.question.answers
+    //answers = choiceButtons
+    // for(var i = 0; i < questions.answers.lenght; i++) {
+      //  choiceButtons.textContent = questions.answers[i].Text; 
+   //  }
 
 }
