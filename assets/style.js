0cc81d00b7e822cea273e4a36999e1aaf79e8103
2dogsandaguy <Neverclear@ymail.com>
2023-07-22 20:51:15 -0400

    added the button to start quiz to pop up

--- assets/style.js
+++ assets/style.js
@@ -1,54 +1,10 @@
-const questions =[
-    {
-        questions:"How old am I",
-        answers: [
-             {text:"29", correct: false},
-             {text:"30", correct: false},
-             {text:"33", correct: true},
-             {text:"37", correct: false}
-        ]
-    },
-    {
-        questions:"am i a good web designer",
-        answers: [
-             {text:"no", correct: false},
-             {text:"maybe", correct: false},
-             {text:"yes", correct: true},
-             {text:"not there yet", correct: false}
-        ]
-    },
-];
-const questionElement = document.getElementById("questions");
-const selectchoice = document.getElementById("select-choice");
-const startButton = document.getElementById("start-quiz");
-startButton.addEventListener("click" , startQuiz);
+var start = document.querySelector(".start")
 
-let currentQuestionIndex = 0;
-let score = 0;
 
-function startQuiz(){
-    currentQuestionIndex = 0;
-    score = 0;
-    //questionElement.textContent=questions[0].questions
-    showQuestions();
-}
 
-function showQuestions(){
-    resetstate();
-    let currentQuestions = questions[currentQuestionIndex];
-    let questionNo = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNo + "." + currentQuestions.
-    questions;
-
-    currentQuestions.answers.forEach( answers => {
-        const button = document.createElement("button");
-        button.innerHTML = answers.text;
-        button.classList.add("btn");
-        selectchoice.appendChild(button);
-
-    })
-}
-
-function resetstate(){
-    
+function startQuiz() { 
+    var element = document.getElementById("hidden");
+    element.classList.add("secshow");
+    start.style.visibility = "hidden";
+    console.log(startQuiz)
 }
