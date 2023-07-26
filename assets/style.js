b5bec335f9f9ba903a3374666d3eee3dfc3c8d26
2dogsandaguy <Neverclear@ymail.com>
2023-07-25 22:43:41 -0400

    git was not showing last push want to send again

--- assets/style.js
+++ assets/style.js
@@ -10,6 +10,7 @@ var timerElement = document.querySelector(".timer-count");
 
 var fillerEl = document.querySelector(".win_loss")
 //var carouselEl = document.getElementById("carousel")
+var displayEl = document.querySelector("large-font")
 
 var win = document.querySelector(".win");
 var lose = document.querySelector(".lose");
@@ -20,115 +21,117 @@ let questions = [
     {
         title: 'where was the first ever comic con held on record ? ',
         answers: [
-             'New York city',
+            'New York city',
             'seattle',
             'sandigio',
-            '900'] ,
+            '900'],
         correctAnnswer: "New York city"
-        
-    },   
+
+    },
     {
         title: 'what is biggest comic con ? ',
         answers: [
-             'kill van kull',
+            'kill van kull',
             'seattle',
             'sandigio',
-            '900'] ,
-            correctAnnswer: "sandigio"
-        
-    }, 
-    {
-        title: 'where does it rain all the time ? ',
-        answers: [
-             'New York city',
-            'seattle',
-            'poop',
-            '900'] ,
-        correctAnnswer: "seattle"
-        
+            '900'],
+        correctAnnswer: "sandigio"
+
     },
     {
         title: 'where does it rain all the time ? ',
         answers: [
-             'New York city',
+            'New York city',
             'seattle',
             'poop',
-            '900'] ,
+            '900'],
         correctAnnswer: "seattle"
-        
+
     },
     {
         title: 'where does it rain all the time ? ',
         answers: [
-             'New York city',
+            'New York city',
             'seattle',
             'poop',
-            '900'] ,
+            '900'],
         correctAnnswer: "seattle"
-        
+
+    },
+    {
+        title: 'where does it rain all the time ? ',
+        answers: [
+            'New York city',
+            'seattle',
+            'poop',
+            '900'],
+        correctAnnswer: "seattle"
+
     },
 ]
- var index = 0;
+var index = 0;
 
 
- $(".btn").each( function () {
-   // var btn = event.target;
-   $(this).on("click", function(event){
-    checkAnwsers(event)
-    index++
-    showQuestions()
-   })
-  // if (btn.classList.includes("next")) {
-   // carousel.next
-   //}
+$(".btn").each(function () {
+    // var btn = event.target;
+    $(this).on("click", function (event) {
+        checkAnwsers(event)
+        index++
+        showQuestions()
+    })
+    // if (btn.classList.includes("next")) {
+    // carousel.next
+    //}
     //if (btn.hasClass("next")) {
-     //   carousel.next();
+    //   carousel.next();
 
     //}
 
-    
+
 });
 
- var timecount = 60;
+var timecount = 60;
 const timerDisplay = () => {
-   var countdown = setInterval(() => {
+    var countdown = setInterval(() => {
         timecount--;
         timerElement.innerHTML = timecount;
-       // if (timecount <= 0) {
+        // if (timecount <= 0) {
 
-       // }
-       
-        if (timecount <= 0){
+        // }
+
+        if (timecount <= 0) {
             clearInterval(countdown);
             endGameHide()
-        }  
-       
-            
+        }
 
-        
+
+
+
     }, 1000);
 }
 
-function checkAnwsers(event){
-console.log(event)
-if (event.target.textContent == questions[index].correctAnnswer){
-    winCounter++;
-    win.textContent = winCounter;
+function checkAnwsers(event) {
+    console.log(event)
+    if (event.target.textContent == questions[index].correctAnnswer) {
+        winCounter++;
+        win.textContent = winCounter;
+        displayEl.textContent = "Correct"
 
+
+    }
+    else {
+        loseCounter++
+        lose.textContent = loseCounter;
+        timecount = timecount - 5;
+    }
+    if (timerElement === 0) {
+        endGameHide()
+
+    }
 }
-else {
-    loseCounter++
-    lose.textContent = loseCounter;
-    timecount = timecount -5;
-}  
- if (timerElement === 0) {
-    endGameHide()
-    
- }
-}
 
 
-function startQuiz() { 
+function startQuiz() {
     var element = document.getElementById("hidden");
     element.classList.add("secshow");
     start.style.display = "none";
@@ -137,40 +140,42 @@ function startQuiz() {
     console.log(startQuiz);
 }
 function getQuestion() {
-    
+
 
 }
-function showQuestions(){
-   // var titleEl = document.getElementById('title');
-     // titleEl.textContent = questions.title;
-    
+var currentQuestionindex = 0
+function showQuestions() {
+
+    // var titleEl = document.getElementById('title');
+    // titleEl.textContent = questions.title;
+
     questionsElement.textContent = questions[index].title;
-     
+
     let choice = document.querySelectorAll('.btn');
-    choice.forEach(function(element, i){
-     element.textContent = questions[index].answers[i]
-     });
-     
+    choice.forEach(function (element, i) {
+        element.textContent = questions[index].answers[i]
+    })
+
+
 }
 var wrapperEl = document.querySelector(".wrapper")
-function endGameHide (){
-    if 
-    (timecount <= 0)
-     wrapperEl.textContent = "GAME OVER LOSER";
-
-}
-function questionsAnswered (){
+function endGameHide() {
     if
-    lastIndexOf.questions = 
+        (timecount <= 0)
+        wrapperEl.textContent = "GAME OVER LOSER";
+
+}
+function questionsAnswered() {
+
 }
 
-function ChoiceQuestions(){
+function ChoiceQuestions() {
 
-//selectChoiceElement.textContent = questions[0].answers;
+    //selectChoiceElement.textContent = questions[0].answers;
     //answers = questions.question.answers
     //answers = choiceButtons
     // for(var i = 0; i < questions.answers.lenght; i++) {
-      //  choiceButtons.textContent = questions.answers[i].Text; 
-   //  }
+    //  choiceButtons.textContent = questions.answers[i].Text; 
+    //  }
 
 }
